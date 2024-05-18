c9faf40e696bf59549cf7708bbbd15b1e5b34a51
Yafik <mhmmdyfik@gmail.com>
2024-05-18 19:11:34 +0700

    add: Flask application with a machine learning model integrated into a Next.js

---
+++ api/index.py
@@ -0,0 +1,20 @@
+from os.path import join
+
+import joblib
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+with open(join("data", "model.pkl"), "rb") as file:
+    model = joblib.load(file)
+
+
+@app.route("/api/predict", methods=["GET"])
+def predict():
+    data = request.get_json()
+    prediction = model.predict([data])
+    return jsonify(prediction.tolist())
+
+
+if __name__ == "__main__":
+    app.run(port=5328)
--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://127.0.0.1:5328/:path*", //backend
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
