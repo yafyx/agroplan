84c55c2cc42174343e7574105f0c8c5a64faba9f
vindiarputra <vindiarptr@gmail.com>
2024-05-24 19:39:00 +0700

    update code

--- api/index.py
+++ api/index.py
@@ -5,7 +5,7 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-with open(join("data", "model.pkl"), "rb") as file:
+with open(join("../data", "model.pkl"), "rb") as file:
     model = joblib.load(file)
 
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,30 @@
+"use client";
+
 import { Input } from "@nextui-org/input";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Home() {
+	const [data, setData] = useState<any>([]);
+	useEffect(() => {
+		// Fetch data from your API
+		async function fetchData() {
+			try {
+				const response = await fetch("http://127.0.0.1:5328/api/predict");
+				if (response.ok) {
+					const data = await response.json();
+					setData(data);
+				} else {
+					console.error("Error fetching data:", response.status);
+				}
+			} catch (error) {
+				console.error("Error fetching data:", error);
+			}
+		}
+
+		fetchData();
+		console.log(data);
+	}, [data]); // Run this effect only once on component mount
+
 	return (
 		<section className="flex flex-wrap items-center justify-center gap-4 px-4 sm:flex-nowrap sm:px-8 sm:gap-8 md:flex-nowrap md:gap-16">
 			<div className="flex flex-col flex-wrap w-full gap-4">
