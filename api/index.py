9889d4000c042df2d02e7b1f140deffd235ae911
Yafik <mhmmdyfik@gmail.com>
2024-05-25 14:08:29 +0700

    chore: Update Flask API endpoint to accept POST requests for prediction

--- api/index.py
+++ api/index.py
@@ -2,17 +2,19 @@ from os.path import join
 
 import joblib
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-with open(join("../data", "model.pkl"), "rb") as file:
+with open(join("..", "data", "model.pkl"), "rb") as file:
     model = joblib.load(file)
 
 
-@app.route("/api/predict", methods=["GET"])
+@app.route("/api/predict", methods=["POST"])
 def predict():
     data = request.get_json()
-    prediction = model.predict([data])
+    prediction = model.predict([list(data.values())])
     return jsonify(prediction.tolist())
 
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,47 +1,121 @@
 "use client";
-
+import { Button } from "@nextui-org/button";
 import { Input } from "@nextui-org/input";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 export default function Home() {
-	const [data, setData] = useState<any>([]);
-	useEffect(() => {
-		// Fetch data from your API
-		async function fetchData() {
-			try {
-				const response = await fetch("http://127.0.0.1:5328/api/predict");
-				if (response.ok) {
-					const data = await response.json();
-					setData(data);
-				} else {
-					console.error("Error fetching data:", response.status);
-				}
-			} catch (error) {
-				console.error("Error fetching data:", error);
-			}
-		}
+  const [n, setN] = useState("");
+  const [p, setP] = useState("");
+  const [k, setK] = useState("");
+  const [ph, setPh] = useState("");
+  const [temperature, setTemperature] = useState("");
+  const [humidity, setHumidity] = useState("");
+  const [rainfall, setRainfall] = useState("");
+  const [prediction, setPrediction] = useState("");
 
-		fetchData();
-		console.log(data);
-	}, [data]); // Run this effect only once on component mount
+  const handleSubmit = async () => {
+    const data = {
+      N: parseFloat(n),
+      P: parseFloat(p),
+      K: parseFloat(k),
+      pH: parseFloat(ph),
+      Temperature: parseFloat(temperature),
+      Humidity: parseFloat(humidity),
+      Rainfall: parseFloat(rainfall),
+    };
 
-	return (
-		<section className="flex flex-wrap items-center justify-center gap-4 px-4 sm:flex-nowrap sm:px-8 sm:gap-8 md:flex-nowrap md:gap-16">
-			<div className="flex flex-col flex-wrap w-full gap-4">
-				<h1 className="text-xl font-semibold sm:text-2xl md:text-4xl">Input</h1>
-				<Input type="text" label="N :" />
-				<Input type="text" label="P :" />
-				<Input type="text" label="pH :" />
-				<Input type="text" label="Temperature :" />
-				<Input type="text" label="Humidity :" />
-				<Input type="text" label="Rainfall :" />
-			</div>
-			<div className="flex flex-col flex-wrap w-full gap-4 ">
-				<h1 className="text-xl font-semibold sm:text-2xl md:text-4xl">Output</h1>
-				<Input type="text" label="N :" disabled value={"Kurang dari 1"} />
-				<Input type="text" label="P :" disabled value={"10"} />
-				<Input type="text" label="K :" disabled value={"10"} />
-			</div>
-		</section>
-	);
+    const response = await fetch("http://localhost:5328/api/predict", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    const result = await response.json();
+    setPrediction(result[0]);
+  };
+
+  return (
+    <section className="flex flex-wrap items-center justify-center gap-4 px-4 sm:flex-nowrap sm:px-8 sm:gap-8 md:flex-nowrap md:gap-16">
+      <div className="flex flex-col flex-wrap w-full gap-4">
+        <h1 className="text-xl font-bold sm:text-2xl md:text-4xl">
+          Soil Predict
+        </h1>
+        <p className="text-gray-500 dark:text-gray-400">
+          Enter the soil parameters:
+        </p>{" "}
+        <div className="grid grid-cols-2 gap-4">
+          <Input
+            type="number"
+            label="N :"
+            labelPlacement="outside"
+            placeholder="Enter"
+            value={n}
+            onChange={(e) => setN(e.target.value)}
+          />
+          <Input
+            type="number"
+            label="P :"
+            labelPlacement="outside"
+            placeholder="Enter"
+            value={p}
+            onChange={(e) => setP(e.target.value)}
+          />
+          <Input
+            type="number"
+            label="K :"
+            labelPlacement="outside"
+            placeholder="Enter"
+            value={k}
+            onChange={(e) => setK(e.target.value)}
+          />
+          <Input
+            type="number"
+            label="pH :"
+            labelPlacement="outside"
+            placeholder="Enter"
+            value={ph}
+            onChange={(e) => setPh(e.target.value)}
+          />
+          <Input
+            type="number"
+            label="Temperature :"
+            labelPlacement="outside"
+            placeholder="Enter"
+            value={temperature}
+            onChange={(e) => setTemperature(e.target.value)}
+          />
+          <Input
+            type="number"
+            label="Humidity :"
+            labelPlacement="outside"
+            placeholder="Enter"
+            value={humidity}
+            onChange={(e) => setHumidity(e.target.value)}
+          />
+          <Input
+            type="number"
+            label="Rainfall :"
+            labelPlacement="outside"
+            placeholder="Enter"
+            value={rainfall}
+            onChange={(e) => setRainfall(e.target.value)}
+          />
+        </div>
+        <Button className="w-full" onPress={handleSubmit}>
+          Predict
+        </Button>
+      </div>
+      <div className="flex flex-col flex-wrap w-full gap-4 ">
+        <h1 className="text-xl font-bold sm:text-2xl md:text-4xl">Output</h1>
+        <p className="text-gray-500 dark:text-gray-400">
+          Based on the soil parameters you provided, the recommended is:
+        </p>
+        <div className="rounded-lg bg-gray-100 p-4 text-2xl font-bold dark:bg-gray-800">
+          {prediction}
+        </div>
+      </div>
+    </section>
+  );
 }
