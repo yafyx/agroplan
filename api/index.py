d5a670cabed8b65cc93a71237b39a7e1fbe360a3
Yafik <mhmmdyfik@gmail.com>
2024-05-27 16:22:21 +0700

    Update API index.py and next.config.js

--- api/index.py
+++ api/index.py
@@ -3,10 +3,8 @@ from os.path import join
 
 import joblib
 from flask import Flask, jsonify, request
-from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -61,7 +59,7 @@ def predict():
     }
 
     response = {"predictions": predictions, "comparisons": comparisons}
-
+    logging.debug(f"Response: {response}")
     return jsonify(response)
 
 
