22517609cb6e7c6f9875c84bde3fd2f22b5808d8
vindiarputra <119779436+vindiarputra@users.noreply.github.com>
2024-05-27 14:00:08 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
       {
         source: "/api/:path*",
         destination:
-          process.env.NODE_ENV === "development"
+          process.env.NODE_ENV === "production"
             ? "http://127.0.0.1:5328/api/:path*"
             : "/api/",
       },
