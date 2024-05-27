1124ba6f5970093c0395335ffa8c7d23b1163d14
Yafik <mhmmdyfik@gmail.com>
2024-05-27 08:55:16 +0700

    Update next.config.js for API destination based on environment

--- next.config.js
+++ next.config.js
@@ -4,7 +4,10 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://127.0.0.1:5328/:path*", //backend
+        destination:
+          process.env.NODE_ENV === "development"
+            ? "http://127.0.0.1:5328/api/:path*"
+            : "/api/",
       },
     ];
   },
