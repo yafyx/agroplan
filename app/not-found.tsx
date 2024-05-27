ceaa30adf458c53354ad42d3bae98dd249b66963
Yafik <mhmmdyfik@gmail.com>
2024-05-28 01:01:12 +0700

    Automatic Class Sorting with Prettier

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,9 +1,7 @@
 export default function NotFound() {
   return (
-    <div className="h-[50rem] w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center">
-      <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
-        404 Not Found
-      </p>
-    </div>
+    <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
+      404 Not Found
+    </p>
   );
 }
