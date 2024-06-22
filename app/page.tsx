a65b11e93394b73e9a8b9a2f02cf130561971bdf
vindiarputra <vindiarptr@gmail.com>
2024-06-23 00:19:15 +0700

    add: sensoric system data from thingspeak api endpoint

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,18 @@
 import { Button } from "@nextui-org/button";
 import { Input } from "@nextui-org/input";
 import { Autocomplete, AutocompleteItem } from "@nextui-org/autocomplete";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+
+interface SensorData {
+  pH: number;
+  EC: number;
+  soilMoisture: number;
+  temperature: number;
+  nitrogen: number;
+  phosphorus: number;
+  potassium: number;
+  eh: number;
+}
 
 export default function Home() {
   const [n, setN] = useState("");
@@ -28,6 +39,7 @@ export default function Home() {
   const [hasPrediction, setHasPrediction] = useState(false);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
+  const [select, setSelect] = useState("")
 
   const handleSubmit = async () => {
     if (!n) {
@@ -125,15 +137,90 @@ export default function Home() {
     { label: "Pigeon Peas", value: 23 },
   ];
 
+  const getDataThingspeak = async () => {
+    try {
+      const response = await fetch("https://api.thingspeak.com/channels/2321454/feeds.json?results=1");
+  
+      if (!response.ok) {
+        throw new Error('Failed to fetch sensor data');
+      }
+  
+      const result = await response.json();
+  
+      if (result.feeds.length > 0) {
+        const data = {
+          pH: parseFloat(result.feeds[0].field1),
+          EC: parseFloat(result.feeds[0].field2),
+          soilMoisture: parseInt(result.feeds[0].field3),
+          temperature: parseInt(result.feeds[0].field4),
+          nitrogen: parseInt(result.feeds[0].field5),
+          phosphorus: parseInt(result.feeds[0].field6),
+          potassium: parseInt(result.feeds[0].field7),
+          eh: parseFloat(result.feeds[0].field8),
+        };
+  
+        setN(data.nitrogen.toString());
+        setP(data.phosphorus.toString());
+        setK(data.potassium.toString());
+        setPh(data.pH.toString());
+        setTemperature(data.temperature.toString());
+        setHumidity(data.soilMoisture.toString());
+        setRainfall(data.eh.toString());
+        
+      } else {
+        throw new Error('No sensor data available');
+      }
+    } catch (error) {
+      console.error('Error fetching sensor data:', error);
+    }
+  };
+  
+  useEffect(() => {
+    if (select === "Sensoric System") {
+      getDataThingspeak();
+    }
+  }, [select]);
+  
+
+  const inputOptions = [
+    { label: "Sensoric System"},
+    { label: "Manual Input"},
+  ]
+
+
   return (
-    <section className="flex flex-wrap items-center justify-center gap-4 rounded-2xl bg-white/50 p-8 backdrop-blur-lg dark:bg-zinc-900 sm:flex-nowrap sm:gap-8 sm:px-8 md:flex-nowrap md:gap-16">
-      <div className="flex w-full flex-col flex-wrap gap-4">
+    <section className="flex flex-wrap items-center justify-center gap-4 p-8 rounded-2xl bg-white/50 backdrop-blur-lg dark:bg-zinc-900 sm:flex-nowrap sm:gap-8 sm:px-8 md:flex-nowrap md:gap-16">
+      <div className="flex flex-col flex-wrap w-full gap-4">
         <h1 className="text-xl font-bold sm:text-2xl md:text-4xl">
           Soil Nutrient Prediction
         </h1>
         <p className="text-gray-500 dark:text-gray-400">
           Enter the soil parameters:
         </p>
+        <Autocomplete
+          variant="faded"
+          labelPlacement="outside"
+          label="Select Data Source"
+          placeholder="Sensoric System | Manual Input"
+          className="w-full font-semibold"
+          onSelectionChange={(selected) => {
+            if (selected) {
+              const selectedItem = inputOptions.find(
+                (option) => option.label.toString() === selected,
+              );
+              setSelect(selectedItem ? selectedItem.label.toString() : "");
+            }
+          }}
+        >
+          {inputOptions.map((option) => (
+            <AutocompleteItem
+              key={option.label}
+              value={option.label.toString()}
+            >
+              {option.label}
+            </AutocompleteItem>
+          ))}
+        </Autocomplete>
         <Autocomplete
           variant="faded"
           labelPlacement="outside"
@@ -168,6 +255,7 @@ export default function Home() {
           value={n}
           onChange={(e) => setN(e.target.value)}
           className="font-semibold"
+          isDisabled={select === "Sensoric System"}
         />
         <Input
           variant="faded"
@@ -178,6 +266,7 @@ export default function Home() {
           placeholder="Enter"
           value={p}
           onChange={(e) => setP(e.target.value)}
+          isDisabled={select === "Sensoric System"}
         />
         <Input
           variant="faded"
@@ -189,6 +278,7 @@ export default function Home() {
           value={k}
           onChange={(e) => setK(e.target.value)}
           className="font-semibold"
+          isDisabled={select === "Sensoric System"}
         />
         <div className="grid grid-cols-2 gap-4">
           <Input
@@ -201,6 +291,7 @@ export default function Home() {
             value={ph}
             onChange={(e) => setPh(e.target.value)}
             className="font-semibold"
+            isDisabled={select === "Sensoric System"}
           />
           <Input
             variant="faded"
@@ -212,6 +303,7 @@ export default function Home() {
             value={temperature}
             onChange={(e) => setTemperature(e.target.value)}
             className="font-semibold"
+            isDisabled={select === "Sensoric System"}
           />
           <Input
             variant="faded"
@@ -223,6 +315,7 @@ export default function Home() {
             value={humidity}
             onChange={(e) => setHumidity(e.target.value)}
             className="font-semibold"
+            isDisabled={select === "Sensoric System"}
           />
           <Input
             variant="faded"
@@ -234,12 +327,13 @@ export default function Home() {
             value={rainfall}
             onChange={(e) => setRainfall(e.target.value)}
             className="font-semibold"
+            isDisabled={select === "Sensoric System"}
           />
         </div>
         {error && <p className="font-semibold text-red-500">{error}</p>}
         <Button
           isLoading={loading}
-          className="w-full bg-black text-white dark:bg-white dark:text-black"
+          className="w-full text-white bg-black dark:bg-white dark:text-black"
           onPress={handleSubmit}
         >
           Predict
@@ -257,7 +351,7 @@ export default function Home() {
             Based on the soil parameters and crop selection, the predicted
             nutrient levels are:
           </p>
-          <div className="rounded-lg bg-gray-200 p-2 text-xl font-medium dark:bg-zinc-900/50 sm:p-4">
+          <div className="p-2 text-xl font-medium bg-gray-200 rounded-lg dark:bg-zinc-900/50 sm:p-4">
             <div>
               N:
               <span className="text-4xl font-semibold">
@@ -289,7 +383,7 @@ export default function Home() {
               }
               ), the following recommendations are provided:
             </p>
-            <div className="rounded-lg bg-gray-200 p-2 dark:bg-zinc-900/50 sm:p-4">
+            <div className="p-2 bg-gray-200 rounded-lg dark:bg-zinc-900/50 sm:p-4">
               <table className="w-full">
                 <tbody>
                   <tr className="hover:bg-muted/50 data-[state=selected]:bg-muted border-b border-b-black/20 transition-colors dark:border-b-white/20">
@@ -316,7 +410,7 @@ export default function Home() {
                       {prediction.comparisons.P}
                     </td>
                   </tr>
-                  <tr className="hover:bg-muted/50 transition-colors">
+                  <tr className="transition-colors hover:bg-muted/50">
                     <td>Potassium (K):</td>
                     <td
                       className={`p-2 text-right text-xs sm:text-sm text-${
