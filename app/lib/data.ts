e31506f4a1bdb654db75c791b46a9fc543284991
Yafik <mhmmdyfik@gmail.com>
2024-08-23 11:44:03 +0700

    Moves const into separate file

---
+++ app/lib/data.ts
@@ -0,0 +1,29 @@
+export const inputOptions = [
+    { label: "Sensoric System", value: 1 },
+    { label: "Manual Input", value: 2 },
+];
+
+export const cropOptions = [
+    { label: "Apple", value: 0 },
+    { label: "Banana", value: 1 },
+    { label: "Rice", value: 2 },
+    { label: "Jute", value: 3 },
+    { label: "Watermelon", value: 4 },
+    { label: "Maize", value: 5 },
+    { label: "Chickpea", value: 6 },
+    { label: "Kidney Beans", value: 7 },
+    { label: "Moth Beans", value: 9 },
+    { label: "Mungbean", value: 10 },
+    { label: "Blackgram", value: 11 },
+    { label: "Lentil", value: 12 },
+    { label: "Pomegranate", value: 13 },
+    { label: "Mango", value: 15 },
+    { label: "Grapes", value: 16 },
+    { label: "Muskmelon", value: 17 },
+    { label: "Orange", value: 18 },
+    { label: "Papaya", value: 19 },
+    { label: "Coconut", value: 20 },
+    { label: "Cotton", value: 21 },
+    { label: "Coffee", value: 22 },
+    { label: "Pigeon Peas", value: 23 },
+];
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { Button } from "@nextui-org/button";
 import { Input } from "@nextui-org/input";
 import { Autocomplete, AutocompleteItem } from "@nextui-org/autocomplete";
 import React, { useState } from "react";
+import { cropOptions, inputOptions } from "@/app/lib/data";
 
 export default function Home() {
   const [n, setN] = useState("");
@@ -113,36 +114,6 @@ export default function Home() {
       setLoading(false);
     }
   };
-
-  const cropOptions = [
-    { label: "Apple", value: 0 },
-    { label: "Banana", value: 1 },
-    { label: "Rice", value: 2 },
-    { label: "Jute", value: 3 },
-    { label: "Watermelon", value: 4 },
-    { label: "Maize", value: 5 },
-    { label: "Chickpea", value: 6 },
-    { label: "Kidney Beans", value: 7 },
-    { label: "Moth Beans", value: 9 },
-    { label: "Mungbean", value: 10 },
-    { label: "Blackgram", value: 11 },
-    { label: "Lentil", value: 12 },
-    { label: "Pomegranate", value: 13 },
-    { label: "Mango", value: 15 },
-    { label: "Grapes", value: 16 },
-    { label: "Muskmelon", value: 17 },
-    { label: "Orange", value: 18 },
-    { label: "Papaya", value: 19 },
-    { label: "Coconut", value: 20 },
-    { label: "Cotton", value: 21 },
-    { label: "Coffee", value: 22 },
-    { label: "Pigeon Peas", value: 23 },
-  ];
-
-  const inputOptions = [
-    { label: "Sensoric System", value: 1 },
-    { label: "Manual Input", value: 2 },
-  ];
 
   const getDataThingspeak = async () => {
     try {
